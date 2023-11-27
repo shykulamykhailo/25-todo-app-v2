2eb3299a14d95011df696487ddfd10f4b03553f9
shykulamykhailo <lucamike332@gmail.com>
2023-11-27 20:05:42 +0200

    styles for todoForm

--- src/components/Todos/TodoForm.js
+++ src/components/Todos/TodoForm.js
@@ -1,4 +1,6 @@
 import { useState } from 'react'
+import styles from './TodoForm.module.css'
+
 function TodoForm({ addTodo }) {
   const [text, setText] = useState('')
   const onSubmitHandler = (event) => {
@@ -7,14 +9,16 @@ function TodoForm({ addTodo }) {
     setText('')
   }
   return (
-    <form onSubmit={onSubmitHandler}>
-      <input
-        placeholder="Enter new todo"
-        value={text}
-        onChange={(e) => setText(e.target.value)}
-      ></input>
-      <button type="submit">Submit</button>
-    </form>
+    <div className={styles.todoFormContainer}>
+      <form onSubmit={onSubmitHandler}>
+        <input
+          placeholder="Enter new todo"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        ></input>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
   )
 }
 
