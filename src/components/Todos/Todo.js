ffdb65a57eb829f9bbe9e8f180af9bc1dd025c2c
shykulamykhailo <lucamike332@gmail.com>
2023-11-28 17:17:03 +0200

    add toggleTodo and connect styles

--- src/components/Todos/Todo.js
+++ src/components/Todos/Todo.js
@@ -2,13 +2,24 @@ import { RiDeleteBin2Line, RiTodoFill } from 'react-icons/ri'
 import { FaCheck } from 'react-icons/fa'
 import styles from './Todo.module.css'
 
-function Todo({ todo, deleteTodo }) {
+function Todo({ todo, deleteTodo, toggleTodo }) {
   return (
-    <div className={styles.todo} onDoubleClick={() => deleteTodo(todo.id)}>
+    <div
+      className={`${styles.todo} ${
+        todo.isComplited ? styles.completedTodo : ''
+      }`}
+    >
       <RiTodoFill className={styles.todoIcon} />
       <div className={styles.todoText}>{todo.text}</div>
-      <RiDeleteBin2Line className={styles.deleteIcon} />
-      <FaCheck className={styles.checkIcon} />
+      <RiDeleteBin2Line
+        className={styles.deleteIcon}
+        on
+        onClick={() => deleteTodo(todo.id)}
+      />
+      <FaCheck
+        className={styles.checkIcon}
+        onClick={() => toggleTodo(todo.id)}
+      />
     </div>
   )
 }
